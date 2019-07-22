da60a18500081874ba3730326f21ef5f143583b3
LilianMathu <lilianmathu2018@gmail.com>
2019-07-22 17:55:34 +0300

    setup environment

---
+++ client/src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import Todos from "./Components/Todos";
+
+import './App.css';
+
+class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: "Make dinner",
+        completed: false
+      },
+      {
+        id: 2,
+        title: "walk the dog",
+        completed: false
+      },
+      {
+        id: 3,
+        title: "Buy groceries",
+        completed: false
+      },
+    ]
+  }
+  render() { 
+    console.log(this.state.todos);
+    return (
+      <div className="App">
+        <Todos todos={this.state.todos}/>
+      </div>
+    );
+  }
+}
+
+export default App;
