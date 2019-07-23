d3bf1508338cd7de8e50d50354d7bb6b384ac142
LilianMathu <lilianmathu2018@gmail.com>
2019-07-23 13:30:57 +0300

    add proptypes

--- client/src/Components/TodoItem.js
+++ client/src/Components/TodoItem.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react'
+import PropTypes from "prop-types";
 
-export class TodoItem extends Component {
+class TodoItem extends Component {
     render() {
         return (
-            <div>
+            <div style={{ backgroundColor: '#f4f4f4' }}>
                 <h1>{ this.props.todo.title }</h1>
             </div>
         )
     }
+}
+
+TodoItem.propTypes = {
+    todo: PropTypes.object.isRequired
 }
 
 export default TodoItem;
--- client/src/Components/Todos.js
+++ client/src/Components/Todos.js
@@ -1,11 +1,21 @@
+/* eslint-disable no-lone-blocks */
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 import TodoItem from "./TodoItem";
 
 class Todos extends Component {
     render() {
+      console.log(this.props.todos);
       return this.props.todos.map((todo)=> (
           <TodoItem key={todo.id} todo={ todo }/>
       ));
     }
   }
-  export default Todos;
+
+{ /* Todos is the name of the class. todos is the prop name */ }
+
+Todos.propTypes = {
+  todos: PropTypes.array.isRequired
+}
+
+export default Todos;
